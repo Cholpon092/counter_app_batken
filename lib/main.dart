4ebca7a2c3075de83120559c7376257644e80c28
Cholpon092 <you@example.com>
2023-05-25 19:38:03 +0600

    "git push"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:counter_app/ekinchi_bet.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'app/modules/fist/view/first_view.dart';
 
 void main() {
   runApp(const CounterApp());
@@ -10,126 +12,13 @@ class CounterApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int san = 0;
-  void kemit() {
-    setState(() {
-      san = san - 1;
-    });
-  }
-
-  void kosh() {
-    setState(() {
-      san = san + 1;
-    });
-  }
-
-  void otkoz() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-          builder: (context) => EkinchiBet(
-                sanKelet: san,
-              )),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: Center(
-          child: Text(
-            'Counter App',
-            style: TextStyle(color: Colors.black),
-          ),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12), color: Colors.grey),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 150, vertical: 15),
-              child: Text('San:$san'),
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              GestureDetector(
-                onTap: kemit,
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.blue),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 0),
-                    child: Text(
-                      '-',
-                      style: TextStyle(
-                        fontSize: 60,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              GestureDetector(
-                onTap: kosh,
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.blue),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 35, vertical: 0),
-                    child: Text(
-                      '+',
-                      style: TextStyle(
-                        fontSize: 60,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          TextButton(
-            onPressed: otkoz,
-            child: Icon(
-              Icons.skip_next,
-              size: 55,
-            ),
-          ),
-        ],
-      ),
+      home: FirstView(),
     );
   }
 }
